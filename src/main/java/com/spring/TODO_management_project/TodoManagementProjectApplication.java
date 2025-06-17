5c41049685beeb6e05ba412f40f5aa4a8a8d81d8
dinaahmed10 <codedinaaa@gmail.com>
2025-06-17 17:41:22 +0300

    v1

---
+++ src/main/java/com/spring/TODO_management_project/TodoManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.TODO_management_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementProjectApplication.class, args);
+	}
+
+}
